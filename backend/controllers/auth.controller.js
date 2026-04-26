c788d081bae82abf44dece9fa7e6326b768c54a4
AseelHerzallah1 <herzalla001@gmail.com>
2026-04-26 21:00:49 +0300

    1- user routes and controllers

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs";
 import { generateTokenAndSetCookie } from "../lib/utils/generateToken.js";
-import User from "../modles/user.model.js";
+import User from "../models/user.model.js";
 
 export const signup = async (req, res) => {
     try {
---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,144 @@
+import User from "../models/user.model.js";
+import Notification from "../models/notification.model.js";
+import bcrypt from "bcryptjs";
+import { v2 as cloudinary } from "cloudinary"; 
+
+export const getUserProfile = async (req, res) => {
+    try {
+        const { username } = req.params;
+        const user = await User.findOne({ username }).select("-password -email -__v");
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        } else {
+            return res.status(200).json({ user });
+        }
+    } catch (error) {
+        console.error("Error fetching user profile:", error);
+        return res.status(500).json({ message: "Server error" });
+    }
+};
+
+export const followUnfollowUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const userToModify = await User.findById(id);
+        const currentUser = await User.findById(req.user._id);
+
+        if (id === req.user._id.toString()) {
+            return res.status(400).json({ error: "You cannot follow/unfollow yourself" });
+        }
+        if (!userToModify || !currentUser) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        const isFollowing = currentUser.following.some(f => f.toString() === id);
+
+        if (isFollowing) {
+            await User.findByIdAndUpdate(id, { $pull: { followers: currentUser._id } });
+            await User.findByIdAndUpdate(currentUser._id, { $pull: { following: id } });
+
+            //TODO: return the id of the user as a response 
+            res.status(200).json({ message: "User unfollowed successfully" });
+        } else {
+            await User.findByIdAndUpdate(id, { $push: { followers: currentUser._id } });
+            await User.findByIdAndUpdate(currentUser._id, { $push: { following: id } });
+
+            const newNotification = new Notification({
+                from: currentUser._id,
+                to: userToModify._id,
+                type: "follow"
+            });
+            await newNotification.save();
+
+            //TODO: return the id of the user as a response 
+            res.status(200).json({ message: "User followed successfully" });
+        }
+    } catch (error) {
+        console.error("Error following/unfollowing user:", error);
+        return res.status(500).json({ message: "Server error" });
+    }
+};
+
+export const getSuggestedUsers = async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.user._id);
+        const followingIds = await User.findById(req.user._id).select("following").then(user => user.following);
+
+        const users = await User.aggregate([
+            { $match: { _id: { $ne: currentUser._id, $nin: followingIds } } },
+            { $sample: { size: 10 } },
+            //{ $project: { password: 0, email: 0, __v: 0 } }
+        ]);
+
+        const suggestedUsers = users.slice(0, 4);
+        suggestedUsers.forEach(user => (user.password = null));
+
+        return res.status(200).json({suggestedUsers});
+
+    } catch (error) {
+        console.error("Error fetching suggested users:", error);
+        return res.status(500).json({ message: "Server error" });
+    }
+};
+
+export const updateUser = async (req, res) => {
+    const { fullName, email, username, currentPassword, newPassword, bio, link } = req.body;
+    let { profileImage, coverImage } = req.body;
+    const userId = req.user._id;
+
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        if ((!newPassword && currentPassword) || (newPassword && !currentPassword)) {
+            return res.status(400).json({ message: "Both current and new passwords are required to update the password" });
+        }
+
+        if (newPassword && currentPassword) {
+            const isMatch = await bcrypt.compare(currentPassword, user.password);
+            if (!isMatch) {
+                return res.status(400).json({ message: "Current password is incorrect" });
+            }
+            if (newPassword.length < 6) {
+                return res.status(400).json({ message: "New password must be at least 6 characters long" });
+            }
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(newPassword, salt);
+        }
+
+        if (profileImage) {
+            if (user.profileImage) {
+                await cloudinary.uploader.destroy(user.profileImage.split("/").pop().split(".")[0]);
+            }
+            const uploadedResponse = await cloudinary.uploader.upload(profileImage);
+            profileImage = uploadedResponse.secure_url;
+        }
+
+        if (coverImage) {
+            if (user.coverImage) {
+                await cloudinary.uploader.destroy(user.coverImage.split("/").pop().split(".")[0]);
+            }
+            const uploadedResponse = await cloudinary.uploader.upload(coverImage);
+            coverImage = uploadedResponse.secure_url;
+        }
+
+        user.fullName = fullName || user.fullName;
+        user.email = email || user.email;
+        user.username = username || user.username;
+        user.bio = bio || user.bio;
+        user.link = link || user.link;
+        user.profileImage = profileImage || user.profileImage;
+        user.coverImage = coverImage || user.coverImage;
+
+        const updatedUser = await user.save();
+        // password should not be sent in the response
+        updatedUser.password = null;
+        return res.status(200).json({ user: updatedUser, message: "User updated successfully" });
+
+    } catch (error) {
+        console.error("Error updating user:", error);
+        return res.status(500).json({ message: "Server error" });
+    }
+};
